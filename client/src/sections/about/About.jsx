ff66952b422dfc98d026cccd303b9301fa68d6e9
Lucas Nunes <lucas.jlnunes@gmail.com>
2025-01-14 19:11:25 -0300

    add new image in about

--- client/src/sections/about/About.jsx
+++ client/src/sections/about/About.jsx
@@ -1,5 +1,6 @@
 import "./about.css"
 import { about } from "../../assets"
+import emp from "../../assets/emp.jpeg"
 const About = () => {
   return (
     <section id="about">
@@ -9,11 +10,12 @@ const About = () => {
             <img src={about} alt="" />
           </div>
           <div className="section-header">
-                <h3 className="section-title">Quem Somos</h3>
-                <h1 className="sub-title">Unindo novas tecnologias, com mentes brilhantes</h1>
+                <h1 className="section-title">Quem Somos</h1>
+                <h3 className="sub-title">Unindo novas tecnologias, com mentes brilhantes</h3>
                 <p className="section-description">
                 Com 9 anos de experiência no mercado, somos uma empresa formada pela união de profissionais capacitados. Nosso objetivo não é apenas realizar projetos integrados, mas também oferecer as melhores soluções para edificações a longo prazo. Priorizamos a facilitação da execução, garantindo o menor custo e a maior qualidade em todas as fases do processo.
                 </p>
+                <div className="clients"><img src={emp} alt="" className="client-img"  /></div>
           </div>
         </div>
 

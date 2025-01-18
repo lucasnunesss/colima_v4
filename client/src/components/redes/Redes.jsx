7862b6e6b75774535ecb6dfc62b1d810af74ac3c
Lucas Nunes <lucas.jlnunes@gmail.com>
2025-01-18 03:36:11 -0300

    fixed navbar

--- client/src/components/redes/Redes.jsx
+++ client/src/components/redes/Redes.jsx
@@ -2,7 +2,7 @@ import "./redes.css"
 import { socialNetworks } from "../../../data"
 const Redes = () => {
   return (
-    <div className="flex redes">
+    <div className="flex redes" id="red">
       {socialNetworks.map((handle, index) => (
         <a href={handle.link} className="flex-center icon" target="_blank" key={index}>
           {handle.icon}
